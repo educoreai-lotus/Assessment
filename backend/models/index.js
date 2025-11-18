5725c98565d01194c1defbc5b3b3503ed58ae281
khawla abusaleh <khawlaabusaleh1@gmail.com>
2025-11-18 16:04:27 +0200

    tests: conditional models index without top-level return; mock ExamPackage and ProctoringSession in test mode

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,14 +1,30 @@
-if (process.env.NODE_ENV === 'test') {
-  module.exports = {};
-  return;
-}
+'use strict';
 
-module.exports = {
-  ExamPackage: require('./examPackage.model'),
-  AiAuditTrail: require('./aiAuditTrail.model'),
-  ProctoringEvent: require('./proctoringEvent.model'),
-  ProctoringSession: require('./proctoringSession.model'),
-  ProctoringViolation: require('./proctoringViolation.model'),
-  Incident: require('./incident.model'),
-};
+const isTest = process.env.NODE_ENV === 'test';
+
+if (isTest) {
+  module.exports = {
+    ExamPackage: {
+      findOne: async () => null,
+      findOneAndUpdate: async () => null,
+      create: async () => ({}),
+      save: async () => ({}),
+    },
+    ProctoringSession: {
+      findOne: async () => null,
+      findOneAndUpdate: async () => null,
+      create: async () => ({}),
+      save: async () => ({}),
+    },
+  };
+} else {
+  module.exports = {
+    ExamPackage: require('./examPackage.model'),
+    AiAuditTrail: require('./aiAuditTrail.model'),
+    ProctoringEvent: require('./proctoringEvent.model'),
+    ProctoringSession: require('./proctoringSession.model'),
+    ProctoringViolation: require('./proctoringViolation.model'),
+    Incident: require('./incident.model'),
+  };
+}
 
